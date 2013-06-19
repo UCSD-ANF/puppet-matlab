4edcf5ae82492ef006b23956ebdb9959d3f80ab9
Geoff Davis <gadavis@ucsd.edu>
2013-06-19 11:58:06 -0700

    Add type of license.dat to license
    For use with older versions of matlab that use a license.dat

--- spec/defines/matlab_license_spec.rb
+++ spec/defines/matlab_license_spec.rb
@@ -3,9 +3,32 @@ require 'spec_helper'
 describe 'matlab::license', :type=>'define' do
   let(:title) { 'R2012b' }
   let(:params) { {
-    :source => "puppet:///matlab/license/network.lic",
+    :source => 'puppet:///matlab/license/network.lic',
   } }
 
-  it { should contain_file("Matlab License R2012b").with_path(
-    "/opt/shared/matlab/licenses/network.lic") }
+  it { should contain_file('Matlab License R2012b').with_path(
+    '/opt/shared/matlab/licenses/network.lic')
+  }
+
+  context 'with type = network' do
+    let(:params) { {
+      :source => 'puppet:///matlab/license/network.lic',
+      :type => 'network',
+    } }
+
+    it { should contain_file('Matlab License R2012b').with_path(
+      '/opt/shared/matlab/licenses/network.lic')
+    }
+  end
+  context 'with type = license.dat' do
+    let(:title) { 'R2006b' }
+    let(:params) { {
+      :source => 'puppet:///matlab/license/network.lic',
+      :type => 'license.dat',
+    } }
+
+    it { should contain_file('Matlab License R2006b').with_path(
+      '/opt/shared/matlab/etc/license.dat')
+    }
+  end
 end
